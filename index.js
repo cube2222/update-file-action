aedf8d5d4c90552ea0a6da50a11d66c3a12c48ea
Jakub Martin <jakub.wit.martin@gmail.com>
2021-03-18 13:47:25 +0100

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function run() {
   try {
     const {data: file} = await octokit.repos.getContent({
       owner: 'cube2222',
-      repo: 'octosql',
+      repo: 'testing-spacelift',
       path: targetFilePath,
     });
     sha = file.sha
